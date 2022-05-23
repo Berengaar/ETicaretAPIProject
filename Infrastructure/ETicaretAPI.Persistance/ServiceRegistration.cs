b91996facae06ee74639efd4e73c7bd7e6195742
Berengaar <emir97han@gmail.com>
2022-05-23 16:43:12 +0300

    Project created

---
+++ Infrastructure/ETicaretAPI.Persistance/ServiceRegistration.cs
@@ -0,0 +1,23 @@
+﻿using ETicaretAPI.Application.Abstraction;
+using ETicaretAPI.Persistance.Concretes;
+using ETicaretAPI.Persistance.Contexts;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Persistance
+{
+    //IOC Container
+    public static class ServiceRegistration
+    {
+        public static void AddPersistanceService(this IServiceCollection services)
+        {
+            services.AddSingleton<IProductService, ProductService>();
+            services.AddDbContext<ETicaretAPIDbContext>(options=>options.UseNpgsql("User ID=postgres;Password=123456;Host=localhost;Port=5432;Database=ETicaretAPIDb"));
+        }
+    }
+}
